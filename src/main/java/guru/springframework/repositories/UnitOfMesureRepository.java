b6e3befa5a77b96db7ed730124adff4e903ec99c
gauthamgorla <g1.gautham@gmail.com>
2021-04-06 11:15:28 -0400

    second commit for Udemy

--- src/main/java/guru/springframework/repositories/UnitOfMesureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMesureRepository.java
@@ -4,4 +4,5 @@ import guru.springframework.domain.UnitOfMesure;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UnitOfMesureRepository extends CrudRepository<UnitOfMesure,Long> {
+    //in herer
 }
